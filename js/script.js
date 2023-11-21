6bdec37da66b4904d9d181178367c401b4fde46b
GianmarcoDandrea <gianm96@hotmail.it>
2023-11-21 16:01:59 +0100

    add: bonus to show only complted list

--- js/script.js
+++ js/script.js
@@ -14,14 +14,10 @@ createApp({
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((resp)=> {
-
-                        console.log(resp.data.response);
-                        this.email= resp.data.response
+                        this.email= resp.data.response;
                         this.emailList.push(this.email);
-
                     });
             } 
         }
     }
-    
 }).mount('#app')
